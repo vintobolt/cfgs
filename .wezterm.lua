98cb4f7925b221626cfefc6e2a7dffa2b778299a
Oleksandr Zatserklianyi <vintobolt@outlook.com>
2022-06-24 14:29:48 +0300

    updates..

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,17 +1,21 @@
 local wezterm = require 'wezterm';
 return {
   default_prog = {"/opt/homebrew/bin/fish", "-l"},
-  ---color_scheme = "Urple",
+  ---color_scheme = "PaulMillr",
+  ---color_scheme = "Galizur",
+  ---color_scheme = "Floraverse",
   color_scheme = "UnderTheSea",
   ---color_scheme = "Sakura",
   bold_brightens_ansi_colors = true,
-  window_background_opacity = 0.90,
+  window_background_opacity = 0.96,
   initial_rows = 30,
   initial_cols = 120,
   enable_scroll_bar = true,
   default_cursor_style = "BlinkingBlock", -- Block, Underline, Bar
-  font = wezterm.font("FantasqueSansMono Nerd Font", {weight="Regular", stretch="Normal", italic=false}),
-  font_size = 18.0,
+  ---font = wezterm.font("VictorMono Nerd Font", {weight="Bold", stretch="Normal", italic=false}),
+  ---font = wezterm.font("AurulentSansMono Nerd Font", {weight="Regular", stretch="Normal", italic=false}),
+  font = wezterm.font("Agave Nerd Font", {weight="Regular", stretch="Normal", italic=false}),
+  font_size = 14.0,
   exit_behavior = "CloseOnCleanExit",
   window_frame = {
     font_size = 14.0, 
