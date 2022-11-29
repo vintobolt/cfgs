f5d50379d7a4d4e0a8bd02fbce25a3e35c72d933
Oleksandr Zatserklianyi <46572007+vintobolt@users.noreply.github.com>
2022-11-29 17:54:02 +0200

    update
    rebind SUPER+N to ALT+N because gnome4..

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,21 +1,30 @@
 local wezterm = require 'wezterm';
+--- rebind for gnome
+local act = wezterm.action
+local mykeys = {}
+for i = 1, 8 do
+  table.insert(mykeys, {
+    key = tostring(i),
+    mods = 'ALT',
+    action = act.ActivateTab(i - 1),
+  })
+end
+--- 
+
 return {
-  default_prog = {"/opt/homebrew/bin/fish", "-l"},
-  ---color_scheme = "PaulMillr",
-  ---color_scheme = "Galizur", --- bad selection color
-  ---color_scheme = "UnderTheSea",
-  bold_brightens_ansi_colors = true,
-  window_background_opacity = 0.96,
+  default_prog = {"/usr/bin/fish", "-l"},
   initial_rows = 30,
   initial_cols = 120,
   enable_scroll_bar = true,
-  default_cursor_style = "BlinkingBlock", -- Block, Underline, Bar
-  font = wezterm.font("Agave Nerd Font", {weight="Regular", stretch="Normal", italic=false}),
-  ---font = wezterm.font("Hurmit Nerd Font", {weight="Regular", stretch="Normal", italic=false}),
-  ---font = wezterm.font("TerminessTTF Nerd Font", {weight="Regular", stretch="Normal", italic=false}),
-  font_size = 16.0,
+  default_cursor_style = "BlinkingBlock",
+  font = wezterm.font("CodeNewRoman NF", {weight="Regular", stretch="Normal", italic=false}),
+  font_size = 14.0,
   exit_behavior = "CloseOnCleanExit",
   window_frame = {
-    font_size = 15.0, 
-  } 
+    font_size = 11.0,
+  },
+  color_scheme = "Solarized (light) (terminal.sexy)", -- broken mc colors
+  color_scheme = "Pasque (base16)", -- broken mc colors
+  window_background_opacity = 0.94,
+  keys = mykeys,
 }
