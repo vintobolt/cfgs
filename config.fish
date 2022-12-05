3bcd994acd32172a08d5ad386b84eaeb0ce8b0b2
Oleksandr Zatserklianyi <46572007+vintobolt@users.noreply.github.com>
2022-12-05 21:48:18 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -5,5 +5,11 @@ set -gx PATH "$HOME/.cargo/bin" $PATH
 set -gx PATH "/opt/homebrew/bin" $PATH
 # set -x $GOPATH /home/$USER/go
 # set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
+# linux 
+#set -gx PATH "$HOME/.cargo/bin" $PATH
+#set -x GOROOT /usr/local/go
+#set -x PATH $PATH $GOROOT/bin
+#set -x GOPATH $HOME/go
+#set -x PATH $PATH $GOPATH/bin
 alias ls="exa"
 alias cat="bat"
